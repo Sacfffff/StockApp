a1485418e3e387c0e1295cdb626da7c61b3657bf
Alexandra Kravtsova <merzoyan.sasha@yandex.ru>
2023-10-18 10:15:00 +0300

    Initial commit

---
+++ StockApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StockApp
+//
+//  Created by Alexandra Kravtsova on 18.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StockApp/StockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppApp.swift
+//  StockApp
+//
+//  Created by Alexandra Kravtsova on 18.10.23.
+//
+
+import SwiftUI
+
+@main
+struct StockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
